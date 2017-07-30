9df6d70b7b44c1a04fe3d7ddaa7cbce029da2092
u4bi <u4bi@u4bi.com>
2017-07-30 17:12:38 +0900

    low db setState, getState render

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 var db = low('db');
 
 console.log(db);
+
+
+var data = {
+    users : [
+        { name : 'test', age : 16 },
+
+        { name : 'test', age : 16 },
+
+        { name : 'test', age : 16 },
+
+        { name : 'test', age : 17 },
+
+        { name : 'test', age : 17 }
+        
+    ],
+
+   items : [
+       { name : 'box', price : 400 },
+
+       { name : 'box', price : 400 },
+
+       { name : 'box', price : 400 },
+
+       { name : 'box', price : 500 },
+
+       { name : 'box', price : 500 }
+       
+   ]
+};
+
+db.setState(data);
+
+console.log(db.getState());
